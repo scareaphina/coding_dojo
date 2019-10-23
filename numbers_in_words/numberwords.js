216596ee95d1b008b971145b7ce880bda10655b6
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-10-23 14:43:04 -0400

    Update numberwords.js

--- numbers_in_words/numberwords.js
+++ numbers_in_words/numberwords.js
@@ -10,35 +10,43 @@ three hundred and twenty one
 
 function numberToWord(n) {
 
-var base = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-var tens = ["","", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+  var base = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  var tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
-// 2. create a variable to create a number string
+  // 2. create a variable to create a number string
 
-var nString = n.toString();
+  var nString = n.toString();
 
-// 3. create if statements to deal with negative numbers and zero
+  // 3. create if statements to deal with negative numbers and zero
 
-if (n < 0) return("Error - Negative number.");
-if (n === 0) return("zero");
+  if (n < 0) return ("Error - Negative number.");
+  if (n === 0) return ("zero");
 
-// 4. create an if statement to deal with numbers under 20
-if (n < 20) {
-  return base[n];
-}
+  // 4. create an if statement to deal with numbers under 20
+  if (n < 20) {
+    return base[n];
+  }
 
-// 5. create an if statement for numbers over 20 but under 100
-if (nString.length === 2) {
-  return tens[nString[0]] + " " + base[nString[1]];
-}
+  // 5. create an if statement for numbers over 20 but under 100
+  if (nString.length === 2) {
+    return tens[nString[0]] + " " + base[nString[1]];
+  }
 
-// 6. create an if statement for numbers of over 100
+  // 6. create an if statement for numbers of over 100
+  if (nString.length == 3) {
+    if (nString[1] === "0" && nString[2] === "0")
+      return base[nString[0]] + " hundred";
+    else
+      return base[nString[0]] + " hundred and " + tens[nString[1]] + " " + base[nString[2]];
+  }
+
+// 7. create an if statement for numbers of over 1000
 
 
-// 7. create a function to replace numbers with the words in the array
+// 8. create an if statement for numbers of over 1000000
 
 
-// 8. console log the results
+// 9. console log the results
 
 console.log(n);
 }
