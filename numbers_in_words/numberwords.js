33284239cb95af24746d4d8083bab4335662a308
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-10-23 11:20:40 -0400

    Update numberwords.js

--- numbers_in_words/numberwords.js
+++ numbers_in_words/numberwords.js
@@ -1,15 +1,32 @@
+/*
+
+numberToWord(321);
+three hundred and twenty one
+
+what i need
+1. an array with all of the words for the various numbers
+2. an array for all the number modifiers (hundred, thousand)
+3. something to make the numbers find their equivilant in that array
+4. a function that iterates over the array and expresses the answer in a string
+
+*/
+
 // 1. create an array for number words
 
-var numWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+var base = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+var tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
 // 2. create an array for number modifiers
 
-var numType = ["hundred", "thousand", "million", "billion", "trillion"];
+var numType = ["", "hundred", "thousand", "million", "billion", "trillion"];
 
 // 3. create a function to interate over the numWords array
 
 function numberToWord(n) {
-  
+  if (numWords.includes(n)) {
+    console.log 
+  }
 }
 
 
