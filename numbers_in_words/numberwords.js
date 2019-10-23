cfb9b226eb6d629f2b5419239174e2cfd0247965
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-10-23 11:07:33 -0400

    Create numberwords.js

---
+++ numbers_in_words/numberwords.js
@@ -0,0 +1,19 @@
+// 1. create an array for number words
+
+var numWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+// 2. create an array for number modifiers
+
+var numType = ["hundred", "thousand", "million", "billion", "trillion"];
+
+// 3. create a function to interate over the numWords array
+
+function numberToWord(n) {
+  
+}
+
+
+// 4. create a function to replace numbers with the words in the array
+
+
+// 5. console log the results
