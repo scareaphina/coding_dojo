d931f93884b9214bcca34369350fcc36d6d4f534
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-08 12:54:11 -0500

    Update foobarqix.js

--- kata_FooBarQix/foobarqix.js
+++ kata_FooBarQix/foobarqix.js
@@ -1,20 +1,19 @@
 var output = [];
 var count = [1];
 
-const cases = {
-  Foo: 3,
-  Bar: 5,
-  Qix: 7,
-};
-
-function fooBarQix() {
-  if (count % 3 === 0)
-    output.push("Foo");
-  else if (count % 5 === 0)
-    output.push("Bar");
-  else
-    output.push("*");
-    
-  count++;
-  console.log(output);
+function fooBarQix(value) {
+  if (!value || typeof value !== "string") {
+    return "";
+  } else if (value === "3") {
+    return "Foo" + "Foo";
+  } else if (value === "5") {
+    return "Bar" + "Bar";
+  }
+  return value;
 }
+
+
+console.log(assertEqual(fooBarQix("1"), ""));
+console.log(assertEqual(fooBarQix("3"), "FooFoo"));
+console.log(assertEqual(fooBarQix("2"), "2"));
+console.log(assertEqual(fooBarQix("5"), "BarBar"));
