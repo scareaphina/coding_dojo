3f2b4989346cf0cfe3dfd0c6d8157721c0c94094
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-08 12:07:10 -0500

    Update foobarqix.js

--- kata_FooBarQix/foobarqix.js
+++ kata_FooBarQix/foobarqix.js
@@ -1,1 +1,14 @@
-var 
+var output = [];
+var count = [1];
+
+function fooBarQix() {
+  if (count % 3 === 0)
+    output.push("Foo");
+  else if (count % 5 === 0)
+    output.push("Bar");
+  else
+    output.push("*");
+    
+  count++;
+  console.log(output);
+}
