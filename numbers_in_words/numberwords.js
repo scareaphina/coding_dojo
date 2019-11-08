672a11a9dbd7d56b25fad3a26c02e933c2cb8873
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-08 11:26:09 -0500

    Update numberwords.js

--- numbers_in_words/numberwords.js
+++ numbers_in_words/numberwords.js
@@ -11,28 +11,25 @@ var base = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", 
 var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
 var tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
 
-function negatives(n) {
-  if (n < 0) return ("Error - Negative number.");
-}
-
-function zero(n) {
+function numbertoWord(n) {
   if (n === 0) return ("zero");
+  else if (n < 0) return ("Error - Negative number.");
   else return millions(n);
 }
 
-function singles(n) {
-  if (n < 10) return base[n];
-  else if (n >= 10 && n < 20) return teens[num - 10];
-  else {
-    return tens[Math.floor(n / 10)] + " " + base[num % 10];
+function millions(n) {
+  if (n >= 1000000) {
+    return millions(Math.floor(n / 1000000)) + " million " + thousands(n % 1000000);
+  } else {
+    return thousands(n);
   }
 }
 
-function doubles(n) {
-  if (n < 10) return base[n];
-  else if (n >= 10 && n < 20) return teens[n - 10];
-  else {
-    return tens[Math.floor(n / 10)] + " " + tens[n % 10];
+function thousands(n) {
+  if (n >= 1000) {
+    return hundreds(Math.floor(n / 1000)) + " thousand " + hundreds(n % 1000);
+  } else {
+    return hundreds(n);
   }
 }
 
@@ -44,10 +41,18 @@ function hundreds(n) {
   }
 }
 
-function thousands(n) {
-  if (n >= 1000) {
-    return hundreds(Math.floor(n / 1000)) + " thousand " + hundreds(n % 1000);
-  } else {
-    return hundreds(n);
+function doubles(n) {
+  if (n < 10) return base[n];
+  else if (n >= 10 && n < 20) return teens[n - 10];
+  else {
+    return tens[Math.floor(n / 10)] + " " + base[n % 10];
+  }
+}
+
+function singles(n) {
+  if (n < 10) return base[n];
+  else if (n >= 10 && n < 20) return teens[num - 10];
+  else {
+    return tens[Math.floor(n / 10)] + " " + base[num % 10];
   }
 }
