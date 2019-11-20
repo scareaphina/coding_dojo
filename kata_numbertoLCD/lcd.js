a54b5cd0b58d311b6333b26d2ad63ca854a09937
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-20 11:04:14 -0500

    Create lcd.js

---
+++ kata_numbertoLCD/lcd.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("myTable");
+
+var row = table.insertRow(0);
+
+var cell1 = row.insertCell(0);
+var cell2 = row.insertCell(1);
+
+cell1.innerHtml = "New Cell 1";
+cell2.innerHtml = "New Cell 2";
