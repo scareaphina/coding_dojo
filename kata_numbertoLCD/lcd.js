4c76510acc1cb84c54729c0f809f035a92327397
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-20 12:05:19 -0500

    Update lcd.js

--- kata_numbertoLCD/lcd.js
+++ kata_numbertoLCD/lcd.js
@@ -1,15 +1,25 @@
-function insertRow() {
+function generateRow() {
 
   var table = document.getElementById("myTable").insertRow(0);
 
   var cell1 = table.insertCell(0);
   var cell2 = table.insertCell(1);
+  var cell3 = table.insertCell(2);
 
-  cell1.innerHTML = "New Cell 1";
-  cell2.innerHTML = "New Cell 2";
+  cell1.innerHTML = "";
+  cell2.innerHTML = "";
+  cell3.innerHTML = "";
 
 }
 
+function generateNumber() {
+  var count;
+
+  for (count = 0; count < 3; count++) {
+    generateRow();
+  }
+}
+
 function deleteFunction() {
-  document.getElementById("myTable").deleteRow(0);
+  document.getElementById("myTable").deleteRow(3);
 }
