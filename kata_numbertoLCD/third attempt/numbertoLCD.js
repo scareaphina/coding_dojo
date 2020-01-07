0e1db6c73a4686c886f563839e2bcd93c2230d91
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2020-01-07 15:04:22 -0500

    Update numbertoLCD.js

--- kata_numbertoLCD/third attempt/numbertoLCD.js
+++ kata_numbertoLCD/third attempt/numbertoLCD.js
@@ -5,5 +5,15 @@ function toLCD(n) {
   var shoulders = "";
   var knees = "";
   var toes = "";
-  
+
+  for (var i = 0; i < num.length; i++) {
+    var int = parseInt(num[i], 10);
+
+    head = head + digits[int][0];
+    shoulders = shoulders + digits[int][1];
+    knees = knees + digits[int][2];
+    toes = toes + digits[int][3];
+  }
+
+  return [head, shoulders, knees, toes];
 }
