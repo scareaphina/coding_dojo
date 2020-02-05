b340cb3626b5e8f36d0e204d47c0eca4fbb208fe
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2020-02-05 11:08:11 -0500

    Update anagram.js

--- kata_anagram/anagram.js
+++ kata_anagram/anagram.js
@@ -1,7 +1,19 @@
 // kata Anagram
 
-// take letters from string
+// alphebetize word
+
+function alphebetize(word) {
+  if (!word) {
+    return;
+  }
+  word = word.split('');
+  word = word.sort();
+  word = word.join('');
+  return word;
+}
 
 // compare to list of actual words
+
+const words = [];
 
 // generate two word anagrams
